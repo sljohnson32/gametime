84c88eb00a1612a735d77322d089a2886aa2c129
JeffBuss <turbocapslock@gmail.com>
2016-11-03 13:53:50 -0600

    Finish initial list of tests prior to not taking a nap

--- test/bricks-test.js
+++ test/bricks-test.js
@@ -7,7 +7,7 @@ describe('Bricks', function(){
   context('with default attributes', function() {
     var bricks = new Brick ({});
 
-    it('should be a function', function(){
+    it('should be a function', function() {
       assert.isFunction(Brick);
     });
 
@@ -15,6 +15,20 @@ describe('Bricks', function(){
       assert.equal(Brick.brickArray, []);
     });
 
-    it('should have a brickAlive value of ')
+    it('should have a brickAlive value of true by default', function() {
+      assert.equal(Brick.brickAlive, 'true');
+    });
+
+    it('should add a brick to the column if c < brickColums', function(){
+
+    });
+
+    it('should add a brick to the row if c < brickRows', function(){
+
+    });
+
+    it('should have 4 rows and 8 columns made of bricks w/ width of 70 and height of 25', function () {
+
+    });
   });
 });
