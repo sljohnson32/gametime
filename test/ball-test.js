1c82ea443ca1a2974b7645593d1c250051da3f18
sljohnson32 <sljohnson32@gmail.com>
2016-11-04 17:53:25 -0600

    first ball test

--- test/ball-test.js
+++ test/ball-test.js
@@ -1,10 +1,13 @@
 var chai = require('chai');
 var assert = chai.assert;
-var stub = require('./support/stub');
 var Ball = require('../lib/ball');
 
 describe('Ball', function(){
   context('with default attributes', function() {
+    var ball = new Ball({width: 20, height: 20}, {}, {h: 10});
 
+    it('should be a function', function() {
+      assert.isFunction(ball);
+    });
   });
 });
