643ddd9a2c2e027b3034c099534f4f8c92a16c08
JeffBuss <turbocapslock@gmail.com>
2016-11-08 17:23:03 -0700

    Set multiple describe sections and write descriptions

--- test/ball-test.js
+++ test/ball-test.js
@@ -10,10 +10,6 @@ describe('Ball', function(){
       assert.isFunction(Ball);
     });
 
-    // it('should have a gameOn property set to false when instantiated', function() {
-    //   assert.equal(ball.gameOn, false);
-    // });
-
     it('has a canvas property', function() {
       assert.property(ball.cvs, 'canvas');
     });
@@ -37,9 +33,77 @@ describe('Ball', function(){
     it('should have a y property equal to the canvas height less the paddle height and its radius', function() {
       assert.equal(ball.y, 175);
     });
+  });
+});
 
-    it('should have a property to represent the context object', function() {
+var chai = require('chai');
+var assert = chai.assert;
+var Ball = require('../lib/ball');
+
+describe(' ', function(){
+  context('on action', function() {
+    var ball = new Ball({width: 200, height: 200}, {}, {h: 10});
+
+    it('should be in the center of the paddle if gameMode is 0, 2, or 3', function() {
 
     });
+
+    it('should be moving if gameMode is 1', function() {
+
+    });
+
+
+  });
+});
+
+var chai = require('chai');
+var assert = chai.assert;
+var Ball = require('../lib/ball');
+
+describe(' ', function(){
+  context('on paddleBounce', function() {
+    var ball = new Ball({width: 200, height: 200}, {}, {h: 10});
+
+    it('', function() {
+
+    });
+
+    it('', function() {
+
+    });
+
+    it('', function() {
+
+    });
+
+    it('', function() {
+
+    });
+
+    it('', function() {
+
+    });
+
+
+  });
+});
+
+var chai = require('chai');
+var assert = chai.assert;
+var Ball = require('../lib/ball');
+
+describe(' ', function(){
+  context('on bottomCollision', function() {
+    var ball = new Ball({width: 200, height: 200}, {}, {h: 10});
+
+    it('should return true if ball hits the bottom of the canvas at 1 life', function() {
+
+    });
+
+    it('should return false if ball hits the bottom of the canvas if you have 2 or more lives', function() {
+
+    });
+
+
   });
 });
