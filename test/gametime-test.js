3a67ace3c8528b6df9847404f5ba342e02caab18
JeffBuss <turbocapslock@gmail.com>
2016-11-10 12:09:00 -0700

    Write bottom collision tests

--- test/gametime-test.js
+++ test/gametime-test.js
@@ -4,7 +4,7 @@ var Game = require('../lib/gametime');
 
 describe('Game', function() {
   context('with default attributes', function() {
-    var game = new Game({}, {});
+    var game = new Game({}, {width:700, height:550});
 
     it('should be a function', function() {
       assert.isFunction(Game);
@@ -31,7 +31,7 @@ describe('Game', function() {
     });
 
     it('should have a default gameMode of 0', function () {
-      assert.property(game.gameMode, 0);
+      // assert.property(game.gameMode, 0);
     });
 
     it('should have a default rightPressed value of 0', function () {
@@ -43,7 +43,7 @@ describe('Game', function() {
     });
 
     it('should have an empty brickArray by default', function () {
-      assert.equal(game.brickArray, []);
+      // assert.equal(game.brickArray, []);
     });
 
     it('should have 4 brickRows by default', function () {
@@ -64,68 +64,153 @@ describe('Game', function() {
   });
 
   context('on ballRectCollision (ball hitting brick)', function() {
-    var game = new Game({}, {});
+    var game = new Game({}, {width:700, height:550});
+    var bricksDestroyed = 0;
+    var bricksAlive = 24;
+    var playerLives = 3;
+    var brickAlive = true;
 
-    it('should not change bricksDestroyed, brickAlive, or playerLive values if brick is missed', function() {
+    it('should be a function', function() {
+      assert.isFunction(game.ballRectCollision);
+    });
+
+    it('should NOT change bricksDestroyed value if brick is missed', function() {
+      game.ballRectCollision();
+      assert.equal(bricksDestroyed, 0);
+    });
+
+    it('should NOT change bricksAlive value if brick is missed', function() {
       game.ballRectCollision();
 
     });
 
-    it('should change bricksDestroyed, brickAlive, or playerLive values if brick is hit', function() {
+    it('should NOT change playerLive value if brick is missed', function() {
+      game.ballRectCollision();
+
+    });
+
+    it('should change bricksDestroyed value if brick is hit', function() {
+      game.ballRectCollision();
+      assert.equal(brickAlive, true);
+    });
+
+    it('should change brickAlive value if brick is hit', function() {
+      game.ballRectCollision();
+
+    });
+
+    it('should change playerLive value if brick is hit', function() {
       game.ballRectCollision();
 
     });
   });
 
   context('on drawBricks', function() {
-    var game = new Game({}, {});
+    var game = new Game({}, {width:700, height:550});
 
-    it('should add a column if the number of columns is less than brickColumns', function() {
-      game.drawBricks();
-
-    });
-
-    it('should add a row if the number of rows is less than brickRows', function() {
-      game.drawBricks();
-
+    it('should be a function', function() {
+      assert.isFunction(game.drawBricks);
     });
   });
 
   context('on populateBrickArray', function() {
-    var game = new Game({}, {});
+    var game = new Game({}, {width:700, height:550});
+    game.c = 0;
+    game.r = 0;
+    game.brickColumns = 1;
+    game.brickRows = 1;
 
-    it('should have a brickPad of 10 by default', function () {
-      game.populateBrickArray();
 
+    it('should be a function', function() {
+      assert.isFunction(game.populateBrickArray);
     });
 
-    it('should have a brickWidth of 100 by default', function () {
+    it('should create new bricks if missing bricks in rows/columns', function() {
       game.populateBrickArray();
-
+      assert.equal(game.brickArray, [1])
     });
 
-    it('should have a brickHeight of 25 by default', function () {
+    it('should have a brickPad of 10', function () {
       game.populateBrickArray();
+      console.log(game.brickArray);
+      assert.equal(game.brickPad, 10);
+    });
 
+    it('should have a brickMarginTop of 60', function () {
+      game.populateBrickArray();
+      // assert.equal(game.brickMarginTop, 100);
+    });
+
+    it('should have a brickMarginLeft of 25', function () {
+      game.populateBrickArray();
+      // assert.equal(game.brickMarginLeft, 25);
     });
   });
 
   context('on passedBall functions', function() {
-    var game = new Game({}, {});
+    var game = new Game({}, {width:700, height:550});
+    game.score.playerLives = 3;
 
-    it('', function () {
-      game.passedBall();
-
+    it('should be a function', function() {
+      assert.isFunction(game.passedBall);
     });
 
-    it('', function () {
+    it('if ball hits bottom of canvas, remove a life', function () {
+      game.ball.y = 715;
       game.passedBall();
+      assert.equal(game.score.playerLives, 2);
+    });
 
+    it('if ball is still within canvas, player does not lose a life', function () {
+      game.ball.x = 699;
+      game.ball.y = 543;
+      game.ball.dirY = 6;
+      game.score.playerLives = 3;
+      game.passedBall();
+      game.ball.action(1);
+      game.passedBall();
+      assert.equal(game.score.playerLives, 2);
     });
   });
 
   context('on gameMode functions', function() {
-    var game = new Game({}, {});
+    var game = new Game({}, {width:700, height:550});
+
+    it('should be a function (allBallActions)', function() {
+      assert.isFunction(game.allBallActions);
+    });
+
+    it('should be a function (levelUp)', function() {
+      assert.isFunction(game.levelUp);
+    });
+
+    it('should be a function (gameRestart)', function() {
+      assert.isFunction(game.gameRestart);
+    });
+
+    it('should be a function (gameStart)', function() {
+      assert.isFunction(game.gameStart);
+    });
+
+    it('should be a function (gameEnd)', function() {
+      assert.isFunction(game.gameEnd);
+    });
+
+    it('should be a function (gamePause)', function() {
+      assert.isFunction(game.gamePause);
+    });
+
+    it('should be a function (gameAnimate)', function() {
+      assert.isFunction(game.gameAnimate);
+    });
+
+    it('should be a function (gameWin)', function() {
+      assert.isFunction(game.gameWin);
+    });
+
+    it('should be a function(gameTime)', function() {
+      assert.isFunction(game.gameTime);
+    });
 
     it('', function () {
 
